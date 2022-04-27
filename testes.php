2afed1e5f99a295aef24a00c1994470d080cadc7
lucianogithub <lucianodeoliveira_rj@hotmail.com>
2022-04-26 21:44:13 -0300

    ...

--- testes.php
+++ testes.php
@@ -1,5 +1,14 @@
 <?php
 
+    $nome = 'Luciano de Oliveira Paula';
+    $nome = explode(' ',$nome);
     
+    $sobrenome="";
+    foreach ($nome as $key => $value) {
+        if($key!=0){
+            $sobrenome .= $value." ";
+        }        
+    }
+    echo $sobrenome;
 
 ?>
