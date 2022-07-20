8186ef98320669a22d54045829551edcdf83b2f0
lucianogithub <lucianodeoliveira_rj@hotmail.com>
2022-07-20 17:07:19 -0300

    ...

--- converter.php
+++ converter.php
@@ -11,3 +11,10 @@
 // @readfile($file);
 
 ?>
+
+<!-- <iframe src="https://view.officeapps.live.com/op/embed.aspx?src={https://alcateia.dubsol.app/arquivos/THE%20SANDMAN%20VOLUME%201-01%20O%20sono%20dos%20justos.pdf}"></iframe> -->
+<!-- <iframe src="http://docs.google.com/gview?url={https://alcateia.dubsol.app/arquivos/THE%20SANDMAN%20VOLUME%201-01%20O%20sono%20dos%20justos.pdf}&amp;embedded=true"></iframe> -->
+
+
+        
+        
