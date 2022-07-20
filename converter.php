10bd814f5a3b546cc8e1533423a574cc70c3daec
lucianogithub <lucianodeoliveira_rj@hotmail.com>
2022-07-20 15:36:22 -0300

    ...

---
+++ converter.php
@@ -0,0 +1,13 @@
+<?php
+// AQUI ABRE O ARQUIVO PDF
+
+// $file = 'arquivos/boleto_digital.pdf';
+// $filename = 'Custom file name for the.pdf'; /* Note: Always use .pdf at the end. */
+// header('Content-type: application/pdf');
+// header('Content-Disposition: inline; filename="' . $filename . '"');
+// header('Content-Transfer-Encoding: binary');
+// header('Content-Length: ' . filesize($file));
+// header('Accept-Ranges: bytes');
+// @readfile($file);
+
+?>
